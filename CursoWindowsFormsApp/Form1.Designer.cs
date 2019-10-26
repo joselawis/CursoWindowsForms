cedcf7d9eb2685f267fb69feee98dca312d3234e
Joseluis Contreras Martinez <joselawis@gmail.com>
2019-10-26 16:38:37 +0200

    DataGridView simple

--- CursoWindowsFormsApp/Form1.Designer.cs
+++ CursoWindowsFormsApp/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvCategoria = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCategoria
+            // 
+            this.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategoria.Location = new System.Drawing.Point(12, 118);
+            this.dgvCategoria.Name = "dgvCategoria";
+            this.dgvCategoria.Size = new System.Drawing.Size(400, 150);
+            this.dgvCategoria.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(424, 280);
+            this.Controls.Add(this.dgvCategoria);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mi primera App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvCategoria;
     }
 }
 
--- CursoWindowsFormsApp/Form1.cs
+++ CursoWindowsFormsApp/Form1.cs
@@ -16,5 +16,17 @@ namespace CursoWindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Evento load, se activa cuando esta cargando
+            List<Categoria> listaCategoria = new List<Categoria>
+            {
+                new Categoria {idCategoria=1, nombreCategoria="Fruta"},
+                new Categoria {idCategoria=2, nombreCategoria="Verdura"}
+            };
+
+            dgvCategoria.DataSource = listaCategoria;
+        }
     }
 }
