80bf92c1d5124b9ef12bf9320954dc64f0842b0f
Joseluis Contreras Martinez <joselawis@gmail.com>
2019-11-29 21:28:43 +0100

    Combo box sensitivo

---
+++ CursoWindowsFormsApp/Form6.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CursoWindowsFormsApp
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.lblSeleccione = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(120, 168);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProductos.Size = new System.Drawing.Size(560, 217);
+            this.dgvProductos.TabIndex = 5;
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Location = new System.Drawing.Point(278, 66);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(204, 21);
+            this.cbCategoria.TabIndex = 4;
+            this.cbCategoria.SelectionChangeCommitted += new System.EventHandler(this.filtrar);
+            // 
+            // lblSeleccione
+            // 
+            this.lblSeleccione.AutoSize = true;
+            this.lblSeleccione.Location = new System.Drawing.Point(146, 66);
+            this.lblSeleccione.Name = "lblSeleccione";
+            this.lblSeleccione.Size = new System.Drawing.Size(101, 13);
+            this.lblSeleccione.TabIndex = 3;
+            this.lblSeleccione.Text = "Seleccione un valor";
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.lblSeleccione);
+            this.Name = "Form6";
+            this.Text = "Form6";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label lblSeleccione;
+    }
+}
---
+++ CursoWindowsFormsApp/Form6.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsFormsApp
+{
+    public partial class Form6 : Form
+    {
+        private List<Producto> productos;
+        private List<Categoria> categorias;
+
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            // Combobox
+            categorias = new List<Categoria>
+            {
+                new Categoria {idCategoria=1, nombreCategoria="Fruta"},
+                new Categoria {idCategoria=2, nombreCategoria="Verdura"},
+                new Categoria {idCategoria=3, nombreCategoria="Lapices"},
+                new Categoria {idCategoria=4, nombreCategoria="Gatos"},
+                new Categoria {idCategoria=5, nombreCategoria="Zombies"}
+
+            };
+            cbCategoria.DataSource = categorias;
+            cbCategoria.DisplayMember = "nombreCategoria";
+            cbCategoria.ValueMember = "idCategoria";
+
+            // Data Grid
+            productos = new List<Producto>()
+            {
+                new Producto { idProducto = 1, nombre="Papaya", idCategoria=1 },
+                new Producto { idProducto = 2, nombre="Uva", idCategoria=1},
+                new Producto { idProducto = 3, nombre="Platano", idCategoria=1},
+                new Producto { idProducto = 4, nombre="Lechuga", idCategoria=2},
+                new Producto { idProducto = 5 , nombre="Cebolla", idCategoria=2},
+                new Producto { idProducto = 6, nombre="Ajo", idCategoria=2}
+            };
+            dgvProductos.DataSource = productos;
+        }
+
+        private void filtrar(object sender, EventArgs e)
+        {
+            int id = int.Parse(cbCategoria.SelectedValue.ToString());
+            IEnumerable<Producto> consulta = from producto in productos
+                                             where producto.idCategoria.Equals(id)
+                                             select producto;
+
+            dgvProductos.DataSource = consulta.ToList<Producto>();
+        }
+    }
+}
